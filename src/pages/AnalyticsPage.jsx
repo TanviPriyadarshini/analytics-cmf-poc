26a0c8fd5a2414226f92a4c472a35a7d83ed5881
Utkarshbhimte <utkarsh@getmyparking.com>
2018-09-23 14:09:29 +0530

    created Activity Charts

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
-import { LineChart, PieChart } from 'react-chartkick'
-import Chart from 'chart.js'
+import styled from 'styled-components'
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 
+import { Container } from '../sharedComponents/commonStyles'
+
+import { Card } from 'antd'
+
+// data
+import uvData from '../utils/sampleactivityData.json';
+
+const ChartWrap = styled.div`
+    width: inherit;
+    overflow-x: scroll;
+    overflow-y: hidden;
+`
+
+const data = uvData
+    .filter(activity => activity < 500)
+    // TODO: convert the index into timeStamp
+    .map((activity, index) => ({ index: index / 100, activity: parseFloat(activity.toFixed(2)) }))
+
+console.log({ data })
 class AnalyticsPage extends Component {
     render() {
         return (
-            <div>
-                <LineChart data={{ "2017-05-13": 2, "2017-05-14": 5 }} />
-            </div>
+            <Container>
+                <Card title="Activity Chart">
+                    <ChartWrap>
+                        <LineChart width={data.length * 5} height={250} data={data}>
+                            {/* <XAxis /> */}
+                            <YAxis />
+                            <Tooltip />
+                            <Line
+                                activeDot={{ r: 1 }}
+                                type="monotone" dataKey="activity" stroke="#8884d8"
+                            />
+                        </LineChart>
+                    </ChartWrap>
+                </Card>
+            </Container>
         )
     }
 }
