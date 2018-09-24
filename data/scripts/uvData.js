749cdd90b9bec7ba3e0e8692709c2c1cf5ad1e79
Utkarshbhimte <utkarsh@getmyparking.com>
2018-09-25 02:26:10 +0530

    created generateCompletionRate

--- data/scripts/uvData.js
+++ data/scripts/uvData.js
@@ -5,6 +5,22 @@ const UV_DATA_FOLDER = '../uv_data/';
 const VARIANCE = .85
 
 const generateActivityData = data => {
+
+    const avg = (v) => v.reduce((a, b) => a + b, 0) / v.length;
+
+    const smoothOut = (vector, variance) => {
+        var t_avg = avg(vector) * variance;
+        var ret = Array(vector.length);
+        for (var i = 0; i < vector.length; i++) {
+            (function () {
+                var prev = i > 0 ? ret[i - 1] : vector[i];
+                var next = i < vector.length ? vector[i] : vector[i - 1];
+                ret[i] = avg([t_avg, avg([prev, vector[i], next])]);
+            })();
+        }
+        return ret;
+    }
+
     let activityData = data
         .map(([u, v], i) => {
             if (i === 0) return [0, 0];
@@ -19,20 +35,23 @@ const generateActivityData = data => {
     fs.writeFileSync('sampleactivityData.json', JSON.stringify(activityData, null, 2))
 }
 
-const avg = (v) => v.reduce((a, b) => a + b, 0) / v.length;
+const generateCompletionRate = uvDataFiles => {
+    const sessionLengths = uvDataFiles.map(JsonData => JsonData.length)
+    const videoLength = ((5 * 60) + 31) * 10
+    const maxLength = Math.max(...sessionLengths)
 
+    const completionRateObject = {}
+    // adding defaults
+    new Array(10).fill('').map((_, v) => completionRateObject[(v + 1) * 10] = 0)
 
-const smoothOut = (vector, variance) => {
-    var t_avg = avg(vector) * variance;
-    var ret = Array(vector.length);
-    for (var i = 0; i < vector.length; i++) {
-        (function () {
-            var prev = i > 0 ? ret[i - 1] : vector[i];
-            var next = i < vector.length ? vector[i] : vector[i - 1];
-            ret[i] = avg([t_avg, avg([prev, vector[i], next])]);
-        })();
-    }
-    return ret;
+    sessionLengths.forEach(points => {
+        let percent = Math.floor(points * 100 / 3310)
+        percent = percent - (percent % 10)
+
+        completionRateObject[percent]++
+    })
+
+    fs.writeFileSync('completionRateObject.json', JSON.stringify(completionRateObject, null, 2))
 }
 
 const main = async () => {
@@ -40,11 +59,11 @@ const main = async () => {
     const JsonFiles = fs.readdirSync(UV_DATA_FOLDER);
     let uvDataFiles = await Promise.all(JsonFiles.map(async fileName => await require(UV_DATA_FOLDER + fileName)))
 
-    let maxLength = 0
-    uvDataFiles.forEach(JsonData => { maxLength = Math.max(maxLength, JsonData.length) })
+    uvDataFiles = uvDataFiles.filter(fileData => fileData.length < 10000)
 
-    const sampleActivityData = generateActivityData(uvDataFiles[0])
+    generateCompletionRate(uvDataFiles)
 
+    // generateActivityData(uvDataFiles[0])
 }
 
 main()
