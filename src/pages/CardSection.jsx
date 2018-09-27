3ca413f59990e669ecd49f79ac0c1f7532a5e137
utkarshbhimte <utkarshhimte@Utkarshs-MacBook-Pro.local>
2018-09-28 01:08:15 +0530

    created the cards and upload card

--- src/pages/CardSection.jsx
+++ src/pages/CardSection.jsx
@@ -16,9 +16,41 @@ const VideoCard = styled(Card) `
     margin: 1rem 0;
 `
 
+const UploadCard = styled.div`
+    height: calc(100% - 2rem);
+    width: 100%;
+    margin: 1rem 0;
+    border: 4px dashed #212121;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 2rem;
+    transition: all .3s ease-in-out;
+    opacity: .3;
+
+    cursor: pointer;
+
+    &:hover{
+        opacity: .7;
+    }
+`
+
+const videoList = [
+    {
+        id: `wjsb324jk23bn4kj23`,
+        title: `Fistful of Stars`,
+        thumbnail: defaultThumbnail,
+        description: `FISTFUL OF STARS is the world's first Virtual Reality exploration 
+        of the cosmos alongside the Hubble Telescope that transports you inside of the
+        Orion Nebula and reveals the cosmic connections between humans and the stars`
+    }
+]
+
 class CardSection extends Component {
 
-    renderCard = ({ id, thumbnail, title }) => {
+    renderCard = ({ id, thumbnail, title, description }) => {
         const PrimaryBtn = <Link to={`analysis/${id}`}>
             <Icon type="arrow-right" theme="outlined" />
         </Link>
@@ -30,6 +62,7 @@ class CardSection extends Component {
         >
             <Meta
                 title={title}
+                description={description.substring(0, 100) + `...`}
             />
         </VideoCard>
     }
@@ -40,12 +73,14 @@ class CardSection extends Component {
     render() {
         return (
             <Container>
-                <Row gutter={16}>
-                    {new Array(2).fill('').map((_, i) =>
-                        <Col key={i} span={8}>
-                            {this.renderCard({ id: i, thumbnail: defaultThumbnail, title: `Fistful of Stars` })}
-                        </Col>
+                <Row gutter={28} type="flex">
+                    {videoList.map(card =>
+                        <Col key={card.id} span={8}>{this.renderCard(card)} </Col>
                     )}
+                    <Col span={8}>
+                        <UploadCard>Upload Video</UploadCard>
+                    </Col>
+
                 </Row>
             </Container>
         )
